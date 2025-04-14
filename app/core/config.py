11c1fed19fcdd9f7eb4ec55f34242a111edc6cc0
karasy0v <antpahalij@gmail.com>
2025-04-14 23:39:05 +0300

    config read from .env

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic import BaseModel, Field, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunConfig(BaseModel):
@@ -15,9 +15,28 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    hostname: str
+    port: str
+    password: str
+    name: str
+    username: str
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", env_nested_delimiter="__")
+
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
