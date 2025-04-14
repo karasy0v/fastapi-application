93846b479b14ab2c1c098bdc5864807928c5a886
karasy0v <antpahalij@gmail.com>
2025-04-14 19:41:27 +0300

    create main settings

---
+++ app/core/config.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class ApiPrefix(BaseModel):
+    """
+    Prefix to main api router, path: app.api.main_router
+    """
+
+    prefix: str = "/api"
+
+
+class Settings(BaseSettings):
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+
+
+settings = Settings()
