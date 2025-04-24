c8d75ba6b71195cdcb01655eddb8cac634dc9f7a
karasy0v <antpahalij@gmail.com>
2025-04-24 22:25:13 +0300

    add delete cinmea in router cinema

--- app/api/cinema/router.py
+++ app/api/cinema/router.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, Depends, Path
-from sqlalchemy import select
 from app.api.prefixs import CINEMA, TAG_CINEMA
 from app.core.models.db_helper import db_helper as db
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends,  Path
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.crud.cinema import get_cinema, create_new_cinema
+from app.crud.cinema import get_cinema, create_new_cinema, cinema_delete
 from app.api.schemas.cinema import CinemaCreate, CinemaRead
 
 router = APIRouter(prefix=CINEMA, tags=TAG_CINEMA)
@@ -19,3 +18,6 @@ async def get_cinema_by_id(id: int = Path(...),
 async def create_cinema(cinema_data: CinemaCreate, db: AsyncSession = Depends(db.session_getter)):
     return await create_new_cinema(cinema_data,db)
 
+@router.delete('/delete/{id}')
+async def delete_cinema(id: int = Path(...), db: AsyncSession = Depends(db.session_getter)):
+    return await cinema_delete(id,db)
