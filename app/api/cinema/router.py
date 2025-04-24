a653d47206840a0644c177254a4bf6144a832945
karasy0v <antpahalij@gmail.com>
2025-04-24 22:01:51 +0300

    add create cinema in router cinema

--- app/api/cinema/router.py
+++ app/api/cinema/router.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, Path
+from sqlalchemy import select
 from app.api.prefixs import CINEMA, TAG_CINEMA
 from app.core.models.db_helper import db_helper as db
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends,  Path
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.crud.cinema import get_cinema
-from app.api.schemas.cinema import CinemaRead
+from app.crud.cinema import get_cinema, create_new_cinema
+from app.api.schemas.cinema import CinemaCreate, CinemaRead
 
 router = APIRouter(prefix=CINEMA, tags=TAG_CINEMA)
 
@@ -13,3 +14,8 @@ router = APIRouter(prefix=CINEMA, tags=TAG_CINEMA)
 async def get_cinema_by_id(id: int = Path(...),
     db: AsyncSession = Depends(db.session_getter)):
     return await get_cinema(id,db)
+
+@router.post('/create')
+async def create_cinema(cinema_data: CinemaCreate, db: AsyncSession = Depends(db.session_getter)):
+    return await create_new_cinema(cinema_data,db)
+
