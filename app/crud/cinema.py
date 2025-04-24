3d5e1425383e7c6c3fb9eaf1c4b965efa1609dbc
karasy0v <antpahalij@gmail.com>
2025-04-24 22:01:23 +0300

    add func to create new cinema

--- app/crud/cinema.py
+++ app/crud/cinema.py
@@ -2,6 +2,7 @@
 from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.api.schemas.cinema import CinemaCreate
 from app.core.models.models import Cinema 
 
 
@@ -14,3 +15,19 @@ async def get_cinema(id,
     if not result:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not found!')
     return result
+
+async def create_new_cinema(cinema_data: CinemaCreate, session: AsyncSession)  -> Cinema:
+    query_check = select(Cinema).where(Cinema.name == cinema_data.name)
+    result = await session.scalar(query_check)    
+    if result:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'Cinema with name {cinema_data.name} is already exists!')
+    
+    new_cinema = Cinema(
+        name=cinema_data.name
+    )
+    session.add(new_cinema)
+    await session.commit()
+    return{'detail' : 'successfull!'}
+
+
+
