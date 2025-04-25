811d8470b9f03d8a7671995d5dfd0e62074134aa
karasy0v <antpahalij@gmail.com>
2025-04-25 23:55:22 +0300

    create auditorium router

---
+++ app/api/auditorium/router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends, Path
+from app.api.prefixs import AUDITORIUM, TAG_AUDITORIUM
+from app.api.schemas.auditorium import AuditoriumRead
+from app.core.models.db_helper import db_helper as db
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.crud.auditorium import get_auditorium_by_id
+
+
+router = APIRouter(prefix=AUDITORIUM, tags=TAG_AUDITORIUM)
+
+
+@router.get('/{id}', response_model=AuditoriumRead) 
+async def get_audirotium(id: int = Path(...), db: AsyncSession = Depends(db.session_getter)):
+    return await get_auditorium_by_id(id, db)
