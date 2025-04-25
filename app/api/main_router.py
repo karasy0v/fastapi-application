b30a64e5b9dca3b03e31ea49582c5a421304f6da
karasy0v <antpahalij@gmail.com>
2025-04-25 23:55:41 +0300

    include auditorium router to main router

--- app/api/main_router.py
+++ app/api/main_router.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 from app.api.cinema.router import router as cinema_router
+from app.api.auditorium.router import router as auditorium_router
 
 router = APIRouter()
-router.include_router(
-    cinema_router
-    )
+
+routers = [cinema_router,
+    auditorium_router]
+
+for rout in routers:
+    router.include_router(rout)
