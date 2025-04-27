05f070ad73e9922ea0a466fddade105dc6070d16
karasy0v <antpahalij@gmail.com>
2025-04-27 19:35:02 +0300

    rewrite select in auditorium crud func

--- app/crud/auditorium.py
+++ app/crud/auditorium.py
@@ -5,10 +5,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.models.models import Auditorium
 
 async def get_auditorium_by_id(id: int, session: AsyncSession):
-    query_check = select(Auditorium).where(Auditorium.id == id).options(joinedload(Auditorium.cinema))
+    query_check = select(Auditorium).options(joinedload(Auditorium.cinema)).where(Auditorium.id == id)
     result = await session.scalar(query_check)
 
     if not result:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Auditorium is nor found!')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Auditorium is not found!')
     return result
 
