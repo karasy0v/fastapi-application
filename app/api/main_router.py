ba0045dca186676c8e03876ccf7ecc86e2adbe84
karasy0v <antpahalij@gmail.com>
2025-04-28 19:28:50 +0300

    add seat router in main router

--- app/api/main_router.py
+++ app/api/main_router.py
@@ -2,14 +2,16 @@ from fastapi import APIRouter
 from app.api.routers.cinema.router import router as cinema_router
 from app.api.routers.auditorium.router import router as auditorium_router
 from app.api.routers.movies.router import router as movie_router
-from app.api.routers.session.session import router as session_router
+from app.api.routers.session.router import router as session_router
+from app.api.routers.seat.router import router as seat_router
 
 router = APIRouter()
 
 routers = [cinema_router,
     auditorium_router,
     movie_router,
-    session_router]
+    session_router,
+    seat_router]
 
 for rout in routers:
     router.include_router(rout)
