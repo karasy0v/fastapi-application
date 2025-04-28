942069dff409e7d276e8b4e6c806abf4ccd5e5b9
karasy0v <antpahalij@gmail.com>
2025-04-28 16:40:54 +0300

    add session router to main router

--- app/api/main_router.py
+++ app/api/main_router.py
@@ -2,12 +2,14 @@ from fastapi import APIRouter
 from app.api.routers.cinema.router import router as cinema_router
 from app.api.routers.auditorium.router import router as auditorium_router
 from app.api.routers.movies.router import router as movie_router
+from app.api.routers.session.session import router as session_router
 
 router = APIRouter()
 
 routers = [cinema_router,
     auditorium_router,
-    movie_router]
+    movie_router,
+    session_router]
 
 for rout in routers:
     router.include_router(rout)
