b4deed5aca2996effa22e6370c115088622a03c7
karasy0v <antpahalij@gmail.com>
2025-04-28 19:27:48 +0300

    add new prefixs

--- app/api/prefixs.py
+++ app/api/prefixs.py
@@ -5,14 +5,14 @@ TAG_CINEMA = ['Cinemas']
 TAG_AUDITORIUM = ['Auditoriums']
 TAG_MOVIE = ['Movies']
 TAG_SESSION = ['Session']
-
+TAG_SEAT = ['Seat']
 
 
 PREFIX_AUDITORIUM = '/auditorium'
 PREFIX_CINEMA = '/cinema'
 PREFIX_MOVIE = '/movie'
 PREFIX_SESSION = '/session'
-
+PREFIX_SEAT = '/seat'
 
 
 
