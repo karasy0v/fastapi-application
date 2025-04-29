d8a2ca82b7c0813dcf9fa35d2d4c180e904d0360
karasy0v <antpahalij@gmail.com>
2025-04-29 16:47:25 +0300

    add crud funcs to read/post tickets in ticket router

---
+++ app/api/routers/buy_ticket/router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from app.api.prefixs import TAG_TICKET, PREFIX_TICKET
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.api.schemas.ticket import BuyTicket
+from app.core.models.db_helper import db_helper as db
+from app.crud.ticket import get_tickets, buy_tickets
+router = APIRouter(prefix=PREFIX_TICKET, tags= TAG_TICKET)
+
+@router.get('/')
+async def get_ticket(user_id: int, db: AsyncSession = Depends(db.session_getter)):
+    return await get_tickets(user_id, db)
+
+@router.post('/buy')
+async def buy_ticket(ticket_data: BuyTicket, db: AsyncSession = Depends(db.session_getter)):
+    return await buy_tickets(ticket_data, db)
