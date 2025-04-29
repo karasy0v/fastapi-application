550b33151ba3ae9b7fbf47e80a991e4bc11eef05
karasy0v <antpahalij@gmail.com>
2025-04-29 16:46:09 +0300

    create relationships user<-> ticket

--- app/core/models/models.py
+++ app/core/models/models.py
@@ -1,4 +1,3 @@
-from typing import List
 from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column, relationship
@@ -6,14 +5,14 @@ from app.core.models.annotations import (
     int_not_nullable_an,
     str_not_nullable_and_uniq,
     str_not_nullable_an,
-    bool_not_nullable_and_default_false,
     phone_number_an,
     datetime_now_not_nullable_an,
 )
 from sqlalchemy import (
-    ForeignKey,
+    ForeignKey
 )
 from app.core.models.base import Base
+
 
 
 class User(Base):
@@ -23,13 +22,15 @@ class User(Base):
     surname: Mapped[str_not_nullable_an]
     phone_number: Mapped[phone_number_an]
 
+    tickets: Mapped[list["Ticket"]] = relationship(back_populates="user")
+
 
 class Cinema(Base):
     __tablename__ = "cinemas"
 
     name: Mapped[str_not_nullable_and_uniq]
 
-    auditoriums: Mapped[List["Auditorium"]] = relationship(back_populates='cinema')
+    auditoriums: Mapped[list["Auditorium"]] = relationship(back_populates='cinema')
 
 
 class Auditorium(Base):
@@ -48,12 +49,17 @@ class Seat(Base):
     auditorium_id: Mapped[int_not_nullable_an] = mapped_column(ForeignKey("auditoriums.id"))
     row: Mapped[int_not_nullable_an]
     column: Mapped[int_not_nullable_an]
-    busy: Mapped[bool_not_nullable_and_default_false]
+    price: Mapped[int_not_nullable_an]
+
+    __table_args__  = (
+
+    UniqueConstraint("auditorium_id", "row", "column"),
+    
+    )
 
     auditorium: Mapped['Auditorium'] = relationship(back_populates='seats')
 
     
-
 
 class Movie(Base):
     __tablename__ = "movies"
@@ -72,6 +78,7 @@ class Session(Base):
     end_time: Mapped[datetime_now_not_nullable_an]
 
     movie: Mapped["Movie"] = relationship(back_populates='sessions')
+    tickets: Mapped[list["Ticket"]] = relationship(back_populates='session')
 
 
 class Ticket(Base):
@@ -80,3 +87,8 @@ class Ticket(Base):
     price: Mapped[int_not_nullable_an]
     session_id: Mapped[int_not_nullable_an] = mapped_column(ForeignKey("sessions.id"))
     seat_id: Mapped[int_not_nullable_an] = mapped_column(ForeignKey("seats.id"))
+    user_id: Mapped[int_not_nullable_an] = mapped_column(ForeignKey("users.id"))
+
+
+    user: Mapped["User"] = relationship(back_populates="tickets")
+    session: Mapped["Session"] = relationship(back_populates="tickets")
