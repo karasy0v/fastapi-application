7964879edeb1e7e1673cd7d0bab351c5dd44dab2
karasy0v <antpahalij@gmail.com>
2025-05-05 21:14:38 +0300

    add response model

--- app/api/routers/buy_ticket/router.py
+++ app/api/routers/buy_ticket/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from app.api.prefixs import TAG_TICKET, PREFIX_TICKET
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.api.schemas.ticket import BuyTicket
+from app.api.schemas.ticket import BuyTicket, ReadTicket
 from app.core.models.db_helper import db_helper as db
 from app.crud.ticket import get_tickets, buy_tickets
 router = APIRouter(prefix=PREFIX_TICKET, tags= TAG_TICKET)
@@ -10,6 +10,6 @@ router = APIRouter(prefix=PREFIX_TICKET, tags= TAG_TICKET)
 async def get_ticket(user_id: int, db: AsyncSession = Depends(db.session_getter)):
     return await get_tickets(user_id, db)
 
-@router.post('/buy')
+@router.post('/buy', response_model = ReadTicket)
 async def buy_ticket(ticket_data: BuyTicket, db: AsyncSession = Depends(db.session_getter)):
     return await buy_tickets(ticket_data, db)
