8a1d4b0bb175586eccb0d0a4de96773d86493ff2
karasy0v <antpahalij@gmail.com>
2025-05-07 12:15:12 +0300

    delete id from base.py

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -7,8 +7,7 @@ from app.core.config import settings
 class Base(DeclarativeBase):
 
     metadata = MetaData(naming_convention=settings.db.naming_convention)
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-
+   
     def __repr__(self):
         cols = [f"{col}={getattr(self, col)}" for col in self.__table__.columns.keys()]
 
