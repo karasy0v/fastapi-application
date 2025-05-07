bc4aff2017c675f381e919b00bcc6d41485ab512
karasy0v <antpahalij@gmail.com>
2025-05-07 13:41:09 +0300

    create user_id var

--- app/core/models/models.py
+++ app/core/models/models.py
@@ -17,11 +17,12 @@ from fastapi_users.db import (
     SQLAlchemyBaseUserTable,
     SQLAlchemyUserDatabase )
 from app.core.models.base import Base
+from app.core.types.user_id import UserIdType
 from .mixins.id_int_pk import IdIntPkMixin
 
 
 
-class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     __tablename__ = "users"
 
     name: Mapped[str_not_nullable_an]
