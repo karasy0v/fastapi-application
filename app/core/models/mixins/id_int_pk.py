a45e14aa284225427cd92c18f5141c033f2ffc48
karasy0v <antpahalij@gmail.com>
2025-05-07 12:14:43 +0300

    create id_int_pk mixin

---
+++ app/core/models/mixins/id_int_pk.py
@@ -0,0 +1,5 @@
+from sqlalchemy import Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+class IdIntPkMixin:
+     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
