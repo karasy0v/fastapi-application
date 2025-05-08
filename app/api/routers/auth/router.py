4572bb6e4285832a1c0a3b5b64f984c0c905a997
karasy0v <antpahalij@gmail.com>
2025-05-08 19:29:10 +0300

    add register router in auth router

--- app/api/routers/auth/router.py
+++ app/api/routers/auth/router.py
@@ -2,7 +2,19 @@ from fastapi import APIRouter
 from app.api.prefixs import PREFIX_AUTH, TAG_AUTH
 from app.api.dependencies.backend import authentication_backend
 from app.api.dependencies.fastapi_users_router import fastapi_users
+from app.api.schemas.user import UserRead, UserCreate
 
 router = APIRouter(prefix=PREFIX_AUTH, tags=TAG_AUTH)
 
-router.include_router(router=fastapi_users.get_auth_router(authentication_backend))
+router.include_router(
+    router=fastapi_users.get_auth_router(
+        authentication_backend,
+    ),
+)
+
+router.include_router(
+    router=fastapi_users.get_register_router(
+        UserRead,
+        UserCreate,
+    ),
+)
