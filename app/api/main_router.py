19fd201e3d4a6d7dc6f2933a1c3043c79956bb8f
karasy0v <antpahalij@gmail.com>
2025-05-08 18:58:23 +0300

    add auth router to main router

--- app/api/main_router.py
+++ app/api/main_router.py
@@ -5,15 +5,19 @@ from app.api.routers.movies.router import router as movie_router
 from app.api.routers.session.router import router as session_router
 from app.api.routers.seat.router import router as seat_router
 from app.api.routers.buy_ticket.router import router as buy_ticket_router
+from app.api.routers.auth.router import router as auth_router
 
 router = APIRouter()
 
-routers = [cinema_router,
+routers = [
+    cinema_router,
     auditorium_router,
     movie_router,
     session_router,
     seat_router,
-    buy_ticket_router]
+    buy_ticket_router,
+    auth_router,
+]
 
 for rout in routers:
     router.include_router(rout)
